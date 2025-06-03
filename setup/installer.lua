c5524629a967f4fced9ede9f3fb77dc6944e1ac5
paccao <16489683+paccao@users.noreply.github.com>
2025-06-03 23:42:57 +0200

    change stdin api and mode:match
    change stdin to craftOS apis because stdin didnt work
    
    change if statement for checking installer mode since mode:match didnt work

--- setup/installer.lua
+++ setup/installer.lua
@@ -1,10 +1,10 @@
-local stdin = _G.io.stdin
+print('Choose an option (install|update|uninstall)')
 
-local mode = stdin('install, update or uninstall? ')
+local mode = read()
 if not mode then
   error('No input provided')
 end
-if not mode:match('^%s*(install|update|uninstall)%s*$') then
+if mode ~= 'install' and mode ~= 'update' and mode ~= 'uninstall' then
   error('Invalid input, expected "install", "update", or "uninstall"')
 end
 
@@ -16,15 +16,9 @@ if mode == 'uninstall' then
   return print('Uninstalled cctweaked-projects')
 end
 
-local version = stdin('Choose a version (default is main):')
-if not version or version:match('^%s*$') then
-  version = 'main'
-end
-
 print('Downloading Installer...')
 
-local url = "https://raw.githubusercontent.com/paccao/cctweaked-projects/" ..
-    version .. "/remote-inventory/"
+local url = "https://raw.githubusercontent.com/paccao/cctweaked-projects/main/remote-inventory/"
 local req = _G.http.get(url)
 if not req then
   error('Failed to download installer')
