6e493de5223b795fa37730c72cc3a2e54cb47f83
paccao <16489683+paccao@users.noreply.github.com>
2025-06-05 22:36:45 +0200

    loop through each chest in the vicinity for the summary

--- remote-inventory/inventory.lua
+++ remote-inventory/inventory.lua
@@ -4,7 +4,7 @@ for _, name in ipairs(peripheral.getNames()) do
 	local type = peripheral.getType(name)
 	if peripheral.call(name, "list") then
 		print("Found inventory peripheral: " .. name .. " (type: " .. type .. ")")
-		table.insert(chests, peripheral.wrap(type))
+		table.insert(chests, peripheral.wrap(name))
 	end
 end
 
@@ -14,15 +14,18 @@ if #chests == 0 then
 end
 
 local items = {}
-for slot, item in pairs(chest.list()) do
-	if item then
-		if not items[item.name] then
-			items[item.name] = { count = 0, slots = {} }
+for _, chest in ipairs(chests) do
+	for slot, item in pairs(chest.list()) do
+		if item then
+			if not items[item.name] then
+				items[item.name] = { count = 0, slots = {} }
+			end
+			items[item.name].count = items[item.name].count + item.count
+			table.insert(items[item.name].slots, slot)
 		end
-		items[item.name].count = items[item.name].count + item.count
-		table.insert(items[item.name].slots, slot)
 	end
 end
+
 print("Items in the chest:")
 for name, data in pairs(items) do
 	print(name .. ": " .. data.count)
