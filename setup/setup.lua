44301e79f22e1171fd962050d37c269817720d3a
paccao <16489683+paccao@users.noreply.github.com>
2025-06-06 13:03:12 +0200

    fix setup script

--- setup/setup.lua
+++ setup/setup.lua
@@ -1,6 +1,5 @@
 local req = _G.http.get("https://raw.githubusercontent.com/paccao/cctweaked-projects/main/setup/installer.lua")
 local file = _G.fs.open('installer.lua', 'w')
-file.write(req.close())
+file.write(req.readAll())
 file.close()
 req.close()
-return
